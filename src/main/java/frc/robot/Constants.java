49adf55956e1b3c05de7b9611e369be9df1cc303
siiverfish <ivan.g.zimmerman@gmail.com>
2026-01-29 22:59:04 -0800

    turret

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+public class Constants {
+    public class Ports {
+        //public static int TURRET_FLYWHEEL_PORT = syntax error;
+        //public static int TURRET_ROTATE_PORT = i dont know;
+        //public static int TURRET_LOWER_INTAKE_PORT = i dont know;
+        //public static int TURRET_UPPER_INTAKE_PORT = i dont know;
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.Constants.Ports;
+// import com.revrobotics.
+
+public class TurretSubsystem implements Subsystem {
+    
+    // private final Spark rotatorMotor;
+    // private final Spark flywheelMotor;
+    // private final Spark lowerIntakeMotor;
+    // private final Spark upperIntakeMotor;
+
+    // public TurretSubsystem() {
+
+    //     // rotatorMotor = new 
+
+    // }
+
+    // @Override
+    // public void periodic() {
+
+    // }
+}
