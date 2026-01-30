a01f456d620de76c3704620a5a4965bf5a62a2db
siiverfish <ivan.g.zimmerman@gmail.com>
2026-01-29 18:09:03 -0800

    constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,9 +2,9 @@ package frc.robot;
 
 public class Constants {
     public class Ports {
-        //public static int TURRET_FLYWHEEL_PORT = syntax error;
-        //public static int TURRET_ROTATE_PORT = i dont know;
-        //public static int TURRET_LOWER_INTAKE_PORT = i dont know;
-        //public static int TURRET_UPPER_INTAKE_PORT = i dont know;
+        public static int TURRET_FLYWHEEL_PORT = 0;
+        public static int TURRET_ROTATE_PORT = 0;
+        public static int TURRET_LOWER_INTAKE_PORT = 0;
+        public static int TURRET_UPPER_INTAKE_PORT = 0;
     }
 }
