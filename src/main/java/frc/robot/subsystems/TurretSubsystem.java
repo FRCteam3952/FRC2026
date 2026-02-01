a27a7e136c3b404431d65b29329ba2cb22d9acac
siiverfish <ivan.g.zimmerman@gmail.com>
2026-01-31 19:09:03 -0800

    turret updates

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -1,10 +1,15 @@
 package frc.robot.subsystems;
 
+import java.util.Optional;
+
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.Subsystem;
@@ -24,13 +29,17 @@ public class TurretSubsystem implements Subsystem {
     private final RelativeEncoder upperIntakeMotorEncoder;
 
     private final AbsoluteEncoder rotatorMotorAbsoluteEncoder;
-    private double AbsoluteEncoderPos;
+    private final PIDController rotatorPIDController;
+    private double rotatorTargetAngle;
+    
+    private int rotationsSmallGear;
+    private boolean rotatorInitialized;
 
-    private double rotationsSmallGear;
-    private double rotationsBigGear;
+    private double previousRotatorPosition;
+    private boolean previousLimitSwitchValue;
+
 
     public TurretSubsystem() {
-
         rotatorMotor = new SparkMax(Ports.TURRET_ROTATE_PORT, MotorType.kBrushless);
         flywheelMotor = new SparkMax(Ports.TURRET_FLYWHEEL_PORT, MotorType.kBrushless);
         lowerIntakeMotor = new SparkMax(Ports.TURRET_LOWER_INTAKE_PORT, MotorType.kBrushless);
@@ -38,16 +47,32 @@ public class TurretSubsystem implements Subsystem {
         limitSwitch = new DigitalInput(0);
         
         rotatorMotorEncoder = rotatorMotor.getEncoder();
-        rotatorMotorAbsoluteEncoder = rotatorMotor.getAbsoluteEncoder();
         flywheelMotorEncoder = flywheelMotor.getEncoder();
         lowerIntakeMotorEncoder = lowerIntakeMotor.getEncoder();
         upperIntakeMotorEncoder = upperIntakeMotor.getEncoder();
 
-        AbsoluteEncoderPos = rotatorMotorAbsoluteEncoder.getPosition();
+        rotatorMotorAbsoluteEncoder = rotatorMotor.getAbsoluteEncoder();
+        rotatorPIDController = new PIDController(0.2, 0, 0.05); // TODO: tune
+        rotatorPIDController.setTolerance(0.01); // TODO: tune
+
+        rotatorTargetAngle = 0; // TODO: change this to move towards the limit switch?
+
+        rotatorInitialized = false;
+        rotationsSmallGear = 0;
+
+        previousRotatorPosition = rotatorMotorAbsoluteEncoder.getPosition();
+        previousLimitSwitchValue = false; // this has to be false so it can reset at the beginning
+    }
+
+    public boolean atRotatorLimit() {
+        return this.limitSwitch.get();
     }
 
     public void startRotatorMotorRight() {
-        rotatorMotor.set(0.1);
+        // TODO: Should this logic be in startRotatorMotorRight or startRotatorMotorLeft?
+        if (limitSwitch.get() == false) {
+            rotatorMotor.set(0.1);
+        }
     }
 
     public void startRotatorMotorLeft() {
@@ -69,6 +94,7 @@ public class TurretSubsystem implements Subsystem {
     }
 
     public void startFiring() {
+        // TODO: flywheel PID control?
         flywheelMotor.set(0.5);
     }
 
@@ -76,24 +102,51 @@ public class TurretSubsystem implements Subsystem {
         flywheelMotor.set(0);
     }
 
-    public void convertToLargeGearRotations() {
-        // We keep track of the small gear's rotations through the absolute encoder,
-        // but this is not the same as the rotations of the big gear's rotations (due
-        // to the difference in size)
+    public Optional<Rotation2d> tryGetRotatorAngle() {
+        if (!rotatorInitialized) {
+            return Optional.empty();
+        }
+        // One small gear rotation is 1/4 of one big gear rotation.
+        // 0.0 <= exactRotations < 4.0
+        double exactRotations = rotationsSmallGear + rotatorMotorEncoder.getPosition();
+        return Optional.of(Rotation2d.fromDegrees(exactRotations * 90));
+    }
 
-        rotationsBigGear = rotationsSmallGear * 4;
+    public void setRotatorAngle(Rotation2d newTarget) {
+        this.rotatorTargetAngle = newTarget.getRotations();
+    }
+
+    public boolean getAtSetpoint() {
+        return this.rotatorPIDController.atSetpoint();
     }
 
     @Override
     public void periodic() {
-        if (limitSwitch.get()) { // Check to see if the limit switch has been triggered
+        // Check to see if the limit switch has been triggered
+        if (limitSwitch.get() && previousLimitSwitchValue == false) { 
             rotationsSmallGear = 0;
+            rotatorMotorEncoder.setPosition(0);
+            rotatorMotor.stopMotor();
+            rotatorInitialized = true;
         }
-        if (AbsoluteEncoderPos > 0.9 && rotatorMotorAbsoluteEncoder.getPosition() < 0.2) {
+
+        // We finished a rotation, overflowing from a high value like 0.9 back down to 0.1ish
+        if (previousRotatorPosition > 0.8 && rotatorMotorEncoder.getPosition() < 0.2) {
             rotationsSmallGear++;
         }
-        if (AbsoluteEncoderPos < 0.2 && rotatorMotorAbsoluteEncoder.getPosition() > 0.9) {
+        // Opposite case
+        if (previousRotatorPosition < 0.2 && rotatorMotorEncoder.getPosition() > 0.8) {
             rotationsSmallGear--;
+        }
+
+        previousLimitSwitchValue = limitSwitch.get();
+        previousRotatorPosition = rotatorMotorEncoder.getPosition();
+
+        if (rotatorInitialized) {
+            rotatorPIDController.setSetpoint(rotatorTargetAngle);
+            rotatorMotor.set(rotatorPIDController.calculate(tryGetRotatorAngle().get().getRotations()));
+        } else {
+            // TODO: move rotator towards limit switch here, whichever direction that is
         }
     }
 }
